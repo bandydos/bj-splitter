227ab3d40f0969836bc6a815e75e8e761630e091
FelixDer <felix.deryckere@hotmail.com>
2020-08-21 15:51:09 +0200

    cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
 console.log('...');
 
+var suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+var deck = new Array();
+
 const getBeers = async () => {
     const url = '../data/carddata.json';
     const response = await fetch(url);
