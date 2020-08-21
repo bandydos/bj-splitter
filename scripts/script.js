d3e9ed9618196ca21b1ea20bfefb84a82d156bf6
KR44I <38465150+AlexaTack@users.noreply.github.com>
2020-08-21 16:07:55 +0200

    Added suits

--- scripts/script.js
+++ scripts/script.js
@@ -1,60 +1,212 @@
 $(document).ready(() => {
-    console.log('ready');
     $('#btn-generate').click(() => {
-        console.log('clicked')
+        getCards();
     })
 })
 
 const carddata = [{
-    "name": "Ace",
+    "name": "Ace-Clubs",
     "value": 1
 },
 {
-    "name": "Two",
+    "name": "Two-Clubs",
     "value": 2
 },
 {
-    "name": "Three",
+    "name": "Three-Clubs",
     "value": 3
 },
 {
-    "name": "Four",
+    "name": "Four-Clubs",
     "value": 4
 },
 {
-    "name": "Five",
+    "name": "Five-Clubs",
     "value": 5
 },
 {
-    "name": "Six",
+    "name": "Six-Clubs",
     "value": 6
 },
 {
-    "name": "Seven",
+    "name": "Seven-Clubs",
     "value": 7
 },
 {
-    "name": "Eight",
+    "name": "Eight-Clubs",
     "value": 8
 },
 {
-    "name": "Nine",
+    "name": "Nine-Clubs",
     "value": 9
 },
 {
-    "name": "Ten",
+    "name": "Ten-Clubs",
     "value": 10
 },
 {
-    "name": "Jack",
+    "name": "Jack-Clubs",
     "value": 10
 },
 {
-    "name": "Queen",
+    "name": "Queen-Clubs",
     "value": 10
 },
 {
-    "name": "King",
+    "name": "King-Clubs",
+    "value": 10
+},{
+    "name": "Ace-Spades",
+    "value": 1
+},
+{
+    "name": "Two-Spades",
+    "value": 2
+},
+{
+    "name": "Three-Spades",
+    "value": 3
+},
+{
+    "name": "Four-Spades",
+    "value": 4
+},
+{
+    "name": "Five-Spades",
+    "value": 5
+},
+{
+    "name": "Six-Spades",
+    "value": 6
+},
+{
+    "name": "Seven-Spades",
+    "value": 7
+},
+{
+    "name": "Eight-Spades",
+    "value": 8
+},
+{
+    "name": "Nine-Spades",
+    "value": 9
+},
+{
+    "name": "Ten-Spades",
+    "value": 10
+},
+{
+    "name": "Jack-Spades",
+    "value": 10
+},
+{
+    "name": "Queen-Spades",
+    "value": 10
+},
+{
+    "name": "King-Spades",
+    "value": 10
+},{
+    "name": "Ace-Diamonds",
+    "value": 1
+},
+{
+    "name": "Two-Diamonds",
+    "value": 2
+},
+{
+    "name": "Three-Diamonds",
+    "value": 3
+},
+{
+    "name": "Four-Diamonds",
+    "value": 4
+},
+{
+    "name": "Five-Diamonds",
+    "value": 5
+},
+{
+    "name": "Six-Diamonds",
+    "value": 6
+},
+{
+    "name": "Seven-Diamonds",
+    "value": 7
+},
+{
+    "name": "Eight-Diamonds",
+    "value": 8
+},
+{
+    "name": "Nine-Diamonds",
+    "value": 9
+},
+{
+    "name": "Ten-Diamonds",
+    "value": 10
+},
+{
+    "name": "Jack-Diamonds",
+    "value": 10
+},
+{
+    "name": "Queen-Diamonds",
+    "value": 10
+},
+{
+    "name": "King-Diamonds",
+    "value": 10
+},{
+    "name": "Ace-Hearts",
+    "value": 1
+},
+{
+    "name": "Two-Hearts",
+    "value": 2
+},
+{
+    "name": "Three-Hearts",
+    "value": 3
+},
+{
+    "name": "Four-Hearts",
+    "value": 4
+},
+{
+    "name": "Five-Hearts",
+    "value": 5
+},
+{
+    "name": "Six-Hearts",
+    "value": 6
+},
+{
+    "name": "Seven-Hearts",
+    "value": 7
+},
+{
+    "name": "Eight-Hearts",
+    "value": 8
+},
+{
+    "name": "Nine-Hearts",
+    "value": 9
+},
+{
+    "name": "Ten-Hearts",
+    "value": 10
+},
+{
+    "name": "Jack-Hearts",
+    "value": 10
+},
+{
+    "name": "Queen-Hearts",
+    "value": 10
+},
+{
+    "name": "King-Hearts",
     "value": 10
 }];
 
@@ -63,12 +215,9 @@ var suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
 var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 var deck = new Array();
 
-const getBeers = async () => {
-    const url = '../data/carddata.json';
-    const response = await fetch(url);
-    const jsonresponse = await response.json();
-    return jsonresponse;
+const getCards = () => {
+    for(let i = 0; i < carddata.length; i++) {
+        console.log(carddata[i].name)
+    }   
 }
 
-
-console.log(getBeers());
