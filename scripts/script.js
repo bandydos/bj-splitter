6dc8f7902fd5d612869bdfb39cc79336d792d20f
KR44I <38465150+AlexaTack@users.noreply.github.com>
2020-08-23 19:14:27 +0200

    Some frontend + cleanup

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,3 @@
-$(document).ready(() => {
-    $('#btn-generate').click(() => {
-        // Needs some cleaning up.
-        console.log(getCards());
-        console.log(calcTotalValue(cards));
-        console.log(checkOutcome(calcTotalValue(cards)));
-        if (checkHit(calcTotalValue(cards)) == false) {
-            console.log('Stand at ' + calcTotalValue(cards) + '.');
-        }
-    })
-})
-
 const carddata = [{
     "name": "Ace-Clubs",
     "value": 1
@@ -216,6 +204,32 @@ const carddata = [{
     "value": 10
 }];
 
+$(document).ready(() => {
+    $('#table-cards-1').hide();
+    $('#table-cards-2').hide();
+    $('#table-cards-3').hide();
+
+    $('#btn-shuffle').click(() => {
+        shuffle();
+        displayControl('s');
+        showCards(cards, 'table-cards-1');
+        updateTotal(cards, 'h-total-1');
+        updateOutcome('h-outcome-1')
+        updateStand('h-stand-1', checkHit(calcTotalValue(cards)));
+    })
+    $('#btn-hit').click(() => {
+        getCard();
+        showCards(cards, 'table-cards-1');
+        displayControl('h');
+        updateTotal(cards, 'h-total-1');
+        updateOutcome('h-outcome-1')
+        updateStand('h-stand-1', checkHit(calcTotalValue(cards)));
+    })
+    $('#btn-reset').click(() => {
+        displayControl('r');
+        cards = [];
+    })
+})
 
 var suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
 var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
@@ -239,15 +253,15 @@ const makeDeck = () => {
 var cards = [];
 
 const checkHit = (totalValue) => {
-    if ((totalValue < 15)) {
-        return true; // Hit when total < 15.
+    if ((totalValue < 16)) {
+        return true; // Hit when total < 16.
     } else {
         return false;
     }
 }
 
 
-const getCards = () => {
+const getCard = () => {
     const hit = checkHit(calcTotalValue(cards));
 
     if (hit) {
@@ -273,13 +287,75 @@ const checkOutcome = (totalValue) => {
     let outcome = "";
 
     // Possible outcomes.
-    if (totalValue > 15 && totalValue < 21) outcome = 'G'; // Good hand.
-    else if (totalValue > 21) outcome = 'B'; // Bust.
-    else if (totalValue == 21) outcome = 'BJ'; // Blackjack.
-    else outcome = 'L'; // Low hand.
+    if (totalValue > 15 && totalValue < 21) outcome = 'Good hand'; // Good hand.
+    else if (totalValue > 21) outcome = 'Bust'; // Bust.
+    else if (totalValue == 21) outcome = 'Blackjack'; // Blackjack.
+    else outcome = 'Low hand'; // Low hand.
 
     return outcome;
 }
 
+const showCards = (crds, table) => {
+    $('#table-cards-1 tbody tr').remove(); // Remove old items before adding.
+    for (let i = 0; i < crds.length; i++) {
+        // Append to table.
+        $('#' + table).append(`<tr><td>${i + 1}</td><td>${crds[i].name}</td></tr>`);
+    }
+}
 
+const shuffle = () => {
+    // Shuffle 2 cards.
+    for (let i = 0; i < 2; i++) {
+        getCard();
+    }
+}
+
+const updateTotal = (crds, h) => {
+    // Update the total value.
+    $('#' + h).text(calcTotalValue(crds));
+}
+
+const updateOutcome = (h) => {
+    // Update the outcome status.
+    const outcome = checkOutcome(calcTotalValue(cards));
+    $('#' + h).text(outcome)
+}
+
+const updateStand = (h, hit) => {
+    // Check if hittable.
+    if(hit == false) {
+        $('#' + h).text('Stand');
+    } else {
+        $('#' + h).text('Hittable');
+    }
+}
+
+// Control button displays.
+const displayControl = (type) => {
+    if (type == 's') {
+        $('#btn-shuffle').hide();
+        $('#btn-hit').show();
+        $('#btn-reset').show();
+        $('#table-cards-1').show();
+        $('#h-total-1').show();
+        $('#h-outcome-1').show();
+        $('#h-stand-1').show();
+    } else if (type == 'h') {
+        $('#btn-shuffle').hide();
+        $('#btn-hit').show();
+        $('#btn-reset').show();
+        $('#table-cards-1').show();
+        $('#h-total-1').show();
+        $('#h-outcome-1').show();
+        $('#h-stand-1').show();
+    } else if (type == 'r') {
+        $('#btn-shuffle').show();
+        $('#btn-hit').hide();
+        $('#btn-reset').hide();
+        $('#table-cards-1').hide();
+        $('#h-total-1').hide();
+        $('#h-outcome-1').hide();
+        $('#h-stand-1').hide();
+    }
+}
 
